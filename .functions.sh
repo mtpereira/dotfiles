295ac6ad8798c0d91aff509b4f30743eeef795f5
Manuel Tiago Pereira <mt.pereira@gmail.com>
2019-12-15 00:16:44 +0000

    Add keyboard layout to i3status-rs

--- .functions.sh
+++ .functions.sh
@@ -286,3 +286,15 @@ EOF
   echo "${COMMANDS}" | bluetoothctl
   unset COMMANDS
 }
+
+function show-layout() {
+  local -r layout_code="$(xset -q | grep 'LED' | awk '{print $10}')"
+  local layout_name="??"
+  if [ "${layout_code}" = "00000000" ]; then
+    layout_name="US"
+  elif [ "${layout_code}" = "00001000" ]; then
+    layout_name="PT"
+  fi
+  echo " ${layout_name}"
+}
+
---
+++ .i3/show-layout.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+source ~/.functions.sh
+
+show-layout
+
