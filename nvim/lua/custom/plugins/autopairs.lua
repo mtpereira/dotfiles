76a467fd10311288b4f4b5279cc519e2ce5002fc
Manuel Tiago Pereira <manuel@mtpereira.com>
2023-07-13 15:32:23 +0100

    Update nvim configs using kickstart

---
+++ nvim/lua/custom/plugins/autopairs.lua
@@ -0,0 +1,17 @@
+-- Taken from kickstart's README
+
+return {
+	"windwp/nvim-autopairs",
+	dependencies = { 'hrsh7th/nvim-cmp' },
+	config = function ()
+		require("nvim-autopairs").setup {}
+		local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+		local cmp = require('cmp')
+		cmp.event:on(
+			'confirm_done',
+			cmp_autopairs.on_confirm_done()
+		)
+	end,
+}
+
+-- vim: ts=2 sts=2 sw=2 et
