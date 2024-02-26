d13fd98f91ec4f341ff2e9002a6bd8d67d728aad
Manuel Tiago Pereira <manuel@mtpereira.com>
2024-02-26 18:15:10 +0000

    Do not explicitly define GOPATH
    It will be removed eventually:
    > Go 1.NN (???) will remove the GO111MODULE setting and GOPATH mode entirely,
    > using module mode always.
    https://go.dev/wiki/GOPATH#gopath-variable

--- .env.sh
+++ .env.sh
@@ -6,14 +6,12 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export GPGKEY=EEB5DC0C
 export GPG_TTY=$(tty)
-export GOPATH=$HOME/go
 export GOPROXY="direct"
 export GOENV_ROOT="$HOME/.goenv"
 
 path_add $HOME/bin
 path_add $HOME/bin/scripts
 path_add $HOME/.rbenv/bin
-path_add $GOPATH/bin
 path_add $HOME/go/bin
 path_add $GOENV_ROOT/bin
 path_add $HOME/.cargo/bin
